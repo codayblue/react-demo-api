ab0d3d0d960b2b06965f1a0e6a57fec375cfceb9
codayblue <codayblue@gmail.com>
2017-05-13 14:49:16 -0700

    Finished tasks api and tested. All is working.

--- src/api.php
+++ src/api.php
@@ -1,19 +1,61 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 // Get a controllers collection from the factory
 $api = $app['controllers_factory'];
 
 // Create api routes
 
-$api->get('/', function() use ($app) {
+$api->get('/tasks', function() use ($app) {
     // Get the database connection
     $db = $app['db'];
 
     // Get all the tasks. if there are any.
-    $results = $db->fetchAll('SELECT Name FROM tasks');
+    $tasks = $db->fetchAll('SELECT rowid, Name, Description, Completed FROM tasks WHERE Completed = 0');
 
-    return $app->json($results);   
+    return $app->json($tasks);   
 }); 
 
+$api->get('/tasks/{id}', function($id) use ($app) {
+    // Get DB connection
+    $db = $app['db'];
+
+    // Find the task and return it
+    $task = $db->festAssoc('SELECT rowid, Name, Description, Completed FROM tasks WHERE rowid = ?', array($id));
+    
+    return $app->json($task);
+});
+
+$api->post('/tasks', function(Request $req) use ($app) {
+    // Get DB connection
+    $db = $app['db'];
+
+    // Pull the information out of the request. 
+    $name = $req->get('name');
+    $description = $req->get('description');
+
+    // Create the record
+    $db->insert('tasks', array('Name' => $name, 'Description' => $description, 'Completed' => 0));
+
+    // Send the list of the rows again
+    $tasks = $db->fetchAll('SELECT rowid, Name, Description, Completed FROM tasks WHERE Completed = 0');
+ 
+    // Create the task and send back success
+    return $app->json($tasks);
+});
+
+$api->put('/tasks/{id}', function($id) use ($app) {
+    // Get DB
+    $db = $app['db']; 
+
+    // Complete the Task
+    $db->executeUpdate('UPDATE tasks SET Completed = 1 WHERE rowid = ?', array($id));
+
+    // Send the updated list
+    $tasks = $db->fetchAll('SELECT rowid, Name, Description, Completed FROM tasks WHERE Completed = 0');
+
+    return $app->json($tasks);
+});
 
 return $api;
